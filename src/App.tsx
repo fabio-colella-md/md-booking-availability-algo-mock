679261172d6943d56ff9a15ba91a35d02c55e864
Fabio Colella <fabio.colella@mindoktor.se>
2022-05-18 11:54:53 +0200

    Base complete

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import styles from './App.module.css'
+import initialData from './data.json'
 import { useRef } from 'react'
 import { useEffect } from 'react';
 import { useState } from 'react';
@@ -8,10 +9,35 @@ const range = (s, e = null): number[] =>
     ? [...Array(s).keys()]
     : [...Array(e - s).keys()].map((i) => i + s);
 
-const Canvas = ({ nurses }) => {
-  const [width, height] = [400, 600]
+const timeToInt = (timeString: string) => {
+  const [hourString, minString] = timeString.split(':')
+  const [hours, mins] = [parseInt(hourString), parseInt(minString)]
+  return mins + hours * 60
+}
+
+const Canvas = ({ nurses, availability, bookedSlots, outcome }) => {
+  const [width, height] = [800, 600]
   const bars = nurses + 1
-  const [blockWidth, blockHeight] = [width / 2 / bars, height * 0.8]
+  const [barWidth, barHeight] = [width / 4 / bars * 3, height * 0.8]
+  const availabilityInt = {
+    start: availability ? timeToInt(availability.start) : 0,
+    end: availability ? timeToInt(availability.end) : 1,
+  }
+  const availabilityRange = availabilityInt.end - availabilityInt.start
+
+  const fillAvailabilityTimes = (ctx: CanvasRenderingContext2D) => {
+    ctx.font = '15px sans-serif'
+    ctx.fillStyle = 'black'
+    ctx.fillText(availability.start, 0, height * 0.08)
+    ctx.fillText(availability.end, 0, height * 0.94)
+  }
+
+  const fillOutcomeText = (ctx: CanvasRenderingContext2D) => {
+    const offset = width / bars
+    ctx.font = '15px sans-serif'
+    ctx.fillStyle = 'red'
+    ctx.fillText('Outcome', offset * (bars - 1), height * 0.08)
+  }
 
   const strokeNursesRects = (ctx: CanvasRenderingContext2D) => {
     ctx.strokeStyle = 'black'
@@ -19,13 +45,65 @@ const Canvas = ({ nurses }) => {
     for (const n of range(bars)) {
       if (n === bars -1) {
         ctx.strokeStyle = "red"
+      } else {
+        ctx.font = '15px sans-serif'
+        ctx.fillStyle = 'black'
+        ctx.fillText(`N${n}`, offset * n + barWidth * 0.8, height * 0.08)
       }
-      ctx.strokeRect(offset * n, height * 0.1, blockWidth, blockHeight);
+      ctx.strokeRect(offset * n, height * 0.1, barWidth, barHeight);
     }
   }
-  const addRect = (ctx: CanvasRenderingContext2D) => {
-    ctx.fillStyle = 'black';
-    ctx.strokeRect(10, 10, 10, 10);
+
+  const addSlotBlock = (
+    ctx: CanvasRenderingContext2D, 
+    slot: {start: string, end: string},
+    n,
+    offset: number,
+    barTop: number,
+    extraText: string = '',
+    color: string = 'dodgerblue',
+    fontColor: string = 'white',
+   ) => {
+    const fontSize = 10
+    const [intStart, intEnd] = [
+      timeToInt(slot.start) - availabilityInt.start, 
+      timeToInt(slot.end) - availabilityInt.start
+    ]
+    const [pxStart, pxEnd] = [
+      intStart / availabilityRange * barHeight,
+      intEnd / availabilityRange * barHeight,
+    ]
+    ctx.fillStyle = color
+    ctx.fillRect(offset * n, barTop + pxStart, barWidth, pxEnd - pxStart);
+    ctx.font = `${fontSize}px sans-serif`
+    ctx.fillStyle = fontColor
+    ctx.fillText(slot.start, offset * n + 2, barTop + pxStart + fontSize)
+    extraText && ctx.fillText(extraText, offset * n + 2, barTop + pxStart + ((pxEnd - pxStart) / 2) + fontSize / 3)
+    ctx.fillText(slot.end, offset * n + 2, barTop + pxStart + (pxEnd - pxStart) - fontSize / 3)
+  }
+
+  const addBookedSlots = (ctx: CanvasRenderingContext2D) => {
+    const offset = width / bars
+    const barTop = height * 0.1
+    for (const n of range(bookedSlots.length)) {
+      const currentNurseSlots = bookedSlots[n]
+      for (const slot of currentNurseSlots) {
+        if (slot && slot.start && slot.end) {
+          addSlotBlock(ctx, slot, n, offset, barTop, 'Booked')
+        }
+      }
+    }
+  }
+
+  const addOutcomeSlots = (ctx: CanvasRenderingContext2D) => {
+    const offset = width / bars
+    const barTop = height * 0.1
+    for (const slot of outcome) {
+      if (slot && slot.start && slot.end) {
+        addSlotBlock(ctx, slot, bars - 1, offset, barTop, 'Free', 'palegreen', 'black')
+      }
+    }
+    
   }
 
   const canvasRef = useRef<HTMLCanvasElement>(null)
@@ -34,22 +112,22 @@ const Canvas = ({ nurses }) => {
     if (canvas) {
       const ctx = canvas.getContext('2d')
       ctx.clearRect(0, 0, width, height)
+      bookedSlots && addBookedSlots(ctx)
+      outcome && addOutcomeSlots(ctx)
       strokeNursesRects(ctx)
+      availability && fillAvailabilityTimes(ctx)
+      fillOutcomeText(ctx)
+      
+      
+      
     }
-  }, [canvasRef, nurses])
+  }, [canvasRef, nurses, availability, bookedSlots, outcome])
   
   return <canvas className={styles.canvas} ref={canvasRef} width={width} height={height}/>
 }
 
 const App = () => {
-  const [values, setValues] = useState({
-    nurses: 4,
-    bookedSlots: [
-      [{start: "07:00", end: "13:00"}]
-    ],
-    availability: {start: "07:00", end: "18:00"},
-    error: null
-  });
+  const [values, setValues] = useState(initialData);
   const [jsonValues, setJsonValues] = useState(JSON.stringify(values, null, 2));
 
   useEffect(() => {
@@ -71,8 +149,13 @@ const App = () => {
         </div>
       </div>
       <div className={styles.right}>
-        <p>Diagram</p>
-        <Canvas nurses={(values && values["nurses"]) || 0} />
+        <p>Booking Diagram</p>
+        <Canvas 
+          nurses={(values && values["bookedSlots"] ? values["bookedSlots"].length : 0)}
+          availability={(values && values["availability"]) || null}
+          bookedSlots={(values && values["bookedSlots"]) || null}
+          outcome={(values && values["outcome"]) || null}
+        />
       </div>
     </div>
   );
